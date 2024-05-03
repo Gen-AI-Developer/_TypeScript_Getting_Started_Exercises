2371e6502a0c2abdd23a7d92d61b87a4f48e9334
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-05-03 19:54:27 +0500

    third_Working_only_titleCase_Remaining

---
+++ Q3/third.js
@@ -0,0 +1,7 @@
+// 3. Name Cases: Store a person’s name in a variable, and then print 
+// that person’s name in lowercase, uppercase, and titlecase.
+var person_name = "syed safdar ali shah bukhari";
+var splitName = person_name.split(' ');
+console.log(splitName);
+console.log(person_name.toUpperCase);
+console.log(person_name.toLowerCase);
---
+++ Q3/third.ts
@@ -0,0 +1,8 @@
+// 3. Name Cases: Store a person’s name in a variable, and then print 
+// that person’s name in lowercase, uppercase, and titlecase.
+
+let person_name: string = "syed safdar ali shah bukhari";
+let splitName = person_name.split(' ');
+console.log(splitName)
+console.log(person_name.toUpperCase)
+console.log(person_name.toLowerCase)
