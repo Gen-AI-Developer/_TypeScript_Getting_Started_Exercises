34210811beebe50800c9bc2a29df35cd89c2ffdb
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-05-04 23:25:36 +0500

    fourthUpdate

---
+++ Q4/fourth.js
@@ -0,0 +1,3 @@
+var writer_name = "George Carlin";
+var writer_quote = "Never Underestimate the power of stupid people in a large group.";
+console.log("".concat(writer_name, " said once,"), writer_quote);
---
+++ Q4/fourth.ts
@@ -0,0 +1,3 @@
+let writer_name = "George Carlin"
+let writer_quote = "Never Underestimate the power of stupid people in a large group."
+console.log(`${writer_name} said once,`, writer_quote)
