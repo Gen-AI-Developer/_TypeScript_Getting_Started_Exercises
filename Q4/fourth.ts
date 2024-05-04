a107fb45c85c9875a2a72c97cabd4d8b701880a0
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-05-04 23:29:25 +0500

    updatedAll

--- Q4/fourth.ts
+++ Q4/fourth.ts
@@ -1,3 +1,1 @@
-let writer_name = "George Carlin"
-let writer_quote = "Never Underestimate the power of stupid people in a large group."
-console.log(`${writer_name} said once,`, writer_quote)
+console.log(`George Carlinsaid once,Never Underestimate the power of stupid people in a large group.`)
