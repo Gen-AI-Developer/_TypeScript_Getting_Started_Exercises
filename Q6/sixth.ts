951b05b23ecbd4cc15e21a855e074533c0bafdb0
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-05-04 23:36:24 +0500

    sixthUpdate

---
+++ Q6/sixth.ts
@@ -0,0 +1,11 @@
+// 6. Stripping Names: Store a person’s name, and include some whitespace 
+// characters at the beginning and end of the name. Make sure you use each
+// character combination, "\t" and "\n", at least once. Print the name once,
+//  so the whitespace around the name is displayed. Then print the name after 
+// striping the white spaces.
+
+let str = " SafdarAliShah "
+console.log(str)
+str = str.trimStart()
+str = str.trimEnd()
+console.log(str)
