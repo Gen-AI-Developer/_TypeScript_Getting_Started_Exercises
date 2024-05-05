c3a947f7048c1512775f808140f82da4cea9fa71
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-05-05 21:44:09 +0500

    13prob

---
+++ Q11/11.js
@@ -0,0 +1,12 @@
+// 11. Names: Store the names of a few of your friends in a array called names. 
+// Print each person’s name by accessing each element in the list, one at a time.
+var names = [
+    'Safdar',
+    'Ali',
+    'Shah',
+    'Bukhari',
+];
+console.log("First Name of Friend = ", names[0]);
+console.log("Second Name of Friend = ", names[1]);
+console.log("Third Name of Friend = ", names[2]);
+console.log("Fourth Name of Friend = ", names[3]);
---
+++ Q12/12.ts
@@ -0,0 +1,15 @@
+// 12. Greetings: Start with the array you used in Exercise 11, 
+// but instead of just printing each person’s name, print a message to them. 
+// The text of each message
+// should be the same, but each message should be personalized with 
+// the person’s name.
+let friends_names: string[] = [
+    'Safdar',
+    'Ali',
+    'Shah',
+    'Bukhari',
+]
+console.log(`Mr.`, friends_names[0], `You are requested to submit your quiz as soon as possible`)
+console.log(`Also convey this message to`, friends_names[1], `he must submit the assignment`)
+console.log(`Mr.`, friends_names[2], ` Your interview is schduled on Monday, 6th May, 2024`)
+console.log(`Will you be able to convey this to`, friends_names[3])
